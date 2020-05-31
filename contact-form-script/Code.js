904e0d9c35432978dcf176f274af683347e90b1b
Script Raccoon <javascript.martin@gmail.com>
2020-05-31 08:48:42 +0200

    v1

---
+++ contact-form-script/Code.js
@@ -0,0 +1,30 @@
+function doGet() {
+    const output = HtmlService.createTemplateFromFile("index.html").evaluate();
+    output.setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+    return output;
+}
+
+function include(fileName) {
+    return HtmlService.createHtmlOutputFromFile(fileName).getContent();
+}
+
+function sendMessage(text, mail) {
+    if (text === "") {
+        throw new Error("There is no message.");
+    } else if (mail === "") {
+        throw new Error("There is no e-mail.");
+    }
+
+    const body =
+        "A new message has been sent via the contact form.\n\n" +
+        "Message:\n\n" +
+        text +
+        "\n\n" +
+        "E-mail: " +
+        mail;
+    try {
+        GmailApp.sendEmail("javascript.martin@gmail.com", "Contact form submission", body);
+    } catch (e) {
+        throw new Error("The message could not be sent.");
+    }
+}
